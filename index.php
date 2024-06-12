0789b9bb1b586d1f1b29332472bf567b62cb2185
Luca-Mariselli <lucamariselli@gmail.com>
2024-06-12 17:04:16 +0200

    M3 - creata funzione

--- index.php
+++ index.php
@@ -7,10 +7,20 @@
 </head>
 <body>
     <h1>Generatore di Password</h1>
-    <form action="generate_password.php" method="post">
+    <form action="index.php" method="get">
         <label for="length">Lunghezza della password:</label>
         <input type="number" id="length" name="length" min="1" required>
         <button type="submit">Genera Password</button>
     </form>
+
+    <?php
+    if (isset($_GET['length'])) {
+        include 'password_generator.php';
+        $length = intval($_GET['length']);
+        $password = generatePassword($length);
+        echo "<p>Password generata: $password</p>";
+    }
+    ?>
+
 </body>
 </html>
---
+++ password_generator.php
@@ -0,0 +1,14 @@
+<?php
+function generatePassword($length) {
+    $characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+    $charactersLength = strlen($characters);
+    $password = "";
+
+    for ($i = 0; $i < $length; $i++) {
+        $randomIndex = rand(0, $charactersLength - 1);
+        $password .= $characters[$randomIndex];
+    }
+
+    return $password;
+}
+?>
